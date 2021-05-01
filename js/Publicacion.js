91705b0f5d82356bea6320a01bfd4f89f67369ea
danieldiaz-2502 <daniel_diazvalencia@outlook.com>
2021-05-01 18:18:32 -0500

    No me aparecen los comentarios, así no poemos

--- js/Publicacion.js
+++ js/Publicacion.js
@@ -17,11 +17,46 @@ class Publicacion{
         let messageCont = document.createElement('div');
         messageCont.innerHTML = this.publicacion.message;
 
-        let respuestaCont = document.createElement('input');
-        respuestaCont.innerHTML = "Escribe tu publicación";
+        let responderCont = document.createElement('div')
+        responderCont.className = 'responderCont';
+
+
+        let respuestaInput = document.createElement('input');
+        respuestaInput.className = 'respuestaInput';
+
+        let responderBtn = document.createElement('button');
+        responderBtn.className = 'responderBtn';
+        responderBtn.innerHTML = ('Responder');
+
+        let comentariosCont = document.createElement('div')
+        comentariosCont.className = 'comentariosCont';
+
+        responderBtn.addEventListener('click', () => {
+            const publicdb = firebase.database();
+            let texto = respuestaInput.value;
+            console.log(texto);
+            let id = publicdb.ref('publicaciones/'+this.publicacion.id+'/respuestas').push().key;
+
+            let comentario = {
+                id: id,
+                texto: texto,
+            }
+            publicdb.ref('publicaciones/'+this.publicacion.id+'/respuestas/'+id).set(comentario);
+            this.publicacion.innerHTML = (
+
+                '<p>'+'<h3>'+'@'+texto+' ha comentado:'+'</h3>'+'</p>'
+                );
+
+        });
+        
 
         component.appendChild(userCont);
         component.appendChild(messageCont);
+        component.appendChild(responderCont);
+        component.appendChild(comentariosCont);
+        responderCont.appendChild(respuestaInput);
+        responderCont.appendChild(responderBtn);
+
             
             return component;
     }
